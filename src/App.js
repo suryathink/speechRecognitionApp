2e802af9a5db7ce99da2a64a258b4699f945ca3a
Surya Prakash Singh <suryaprakashsingh110@gmail.com>
2023-04-10 18:02:14 +0530

    code added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
+import useClipboard from "react-use-clipboard";
 
 function App() {
+  const [copyText, setCopyText] = useState("");
+  const [isCopied, setCopied] = useClipboard(copyText, {
+    successDuration: 1000,
+  });
+
+  const start = () =>
+    SpeechRecognition.startListening({ continuous: true, language: "en-IN" });
+  const { transcript, browserSupportsSpeechRecognition } =
+    useSpeechRecognition();
+  //  console.log(transcript)
+  const stop = () => {
+    SpeechRecognition.stopListening();
+  };
+
+  if (!browserSupportsSpeechRecognition) {
+    return null;
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <>
+      <div className="container">
+        <h2>Automatic speech recognition</h2>
+        <br />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is my Automatic Speech Recognition web app!
+          With just a click of a button, you can now easily convert your spoken
+          words into text with unmatched speed and accuracy.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <div className="main-content" onClick={() => setCopyText(transcript)}>
+          {transcript}
+          {/* {console.log(transcript)} */}
+        </div>
+
+        <div className="btn-style">
+          <button onClick={setCopied}>
+            {isCopied ? " Text Copied!" : "Copy this Text"}
+          </button>
+          <button onClick={start}>Start Listening</button>
+          <button onClick={SpeechRecognition.stopListening}>
+            Stop Listening
+          </button>
+        </div>
+      </div>
+    </>
   );
 }
 
